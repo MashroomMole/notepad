286e07c3d46540ea9a19e235f12a03f134ebecda
olga.zoldaka <olga.zoldaka@ctco.lv>
2018-11-20 17:06:07 +0200

    111

--- src/lv/ctco/notepad/Main.java
+++ src/lv/ctco/notepad/Main.java
@@ -1,6 +1,8 @@
 package lv.ctco.notepad;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,27 +10,86 @@ public class Main {
 
     static Scanner scanner = new Scanner(System.in);
     static List<Person> records = new ArrayList<>();
+    public static String ask = new String("What you would like to do: create, list, exit or delete?");
 
     public static void main(String[] args) {
         for (; ; ) {
-            System.out.println("cmd: ");
+            System.out.println(ask);
             String cmd = scanner.next();
             switch (cmd) {
-                case "create":
+                case "c":
                     createPerson();
                     break;
-                case "help":
-                    showHelp();
-                    break;
-                case "list":
+//                case "help":
+//                    showHelp();
+//                    break;
+                case "l":
                     list();
                     break;
-                case "exit" ;
+                case "d":
+                    delete();
+                    break;
+                case "e":
                     return;
                 default:
                     System.out.println("Wrong command. Try 'help'");
             }
         }
+
+    }
+
+    private static void createPerson() {
+        Person person = new Person();
+        System.out.println("Please enter name");
+        person.setFirsName(scanner.next());
+        System.out.println("Please enter surname");
+        person.setLastName(scanner.next());
+        System.out.println("Please enter email");
+        person.setEmail(scanner.next());
+        System.out.println("Please enter phone number");
+        person.setPhoneNumber(scanner.next());
+
+
+        records.add(person);
+
+
+    }
+
+    private static void delete() {
+
+        System.out.println("Please enter person's id you'd like to delete");
+        list();
+        int id = scanner.nextInt();
+        int personId = records.indexOf() ;
+
+
+        if (id == personId ) {
+            records.remove(id);
+        } else {
+            System.out.println("id does not exist");
+
+
+        }
+    }
+
+//    private static void loadPersons() {
+//        try (Scanner in = new Scanner(Person)) {
+//            while (in.hasNext()) {
+//                Person person = new Person();
+//                person.getId() = in.nextInt();
+//
+//                results.add(gr);
+//            }
+//
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    private static void list() {
+        records.forEach(r -> System.out.println(r));
 
     }
 }
--- src/lv/ctco/notepad/Person.java
+++ src/lv/ctco/notepad/Person.java
@@ -4,4 +4,66 @@ package lv.ctco.notepad;
  * Created by olga.zoldaka on 11/16/2018.
  */
 public class Person {
+    private static int counter = 0;
+    private int id;
+    private String firsName;
+    private String lastName;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String phoneNumber;
+
+    public Person() {
+        counter++;
+    id = counter;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firsName='" + firsName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
+
